ef058cab48502bf3d88b39d55679b3efa117a418
karthenjamin <karthenjamin@gmail.com>
2017-06-30 09:24:15 -0400

    Add files via upload

---
+++ startup_mac.sh
@@ -0,0 +1,30 @@
+docker stop $(docker ps -q) > /dev/null 2>&1
+docker rm $(docker ps -aq) > /dev/null 2>&1
+docker run -dit --name data_recipes --restart always -p 1234:8888 bbanavige/data_recipes > /dev/null
+echo ""
+echo "-----------------------------------------------------------------------------------"
+echo "opening jupyter notebook"
+echo "-----------------------------------------------------------------------------------"
+echo ""
+sleep 2
+open "http://0.0.0.0:1234/?token=benkarthik"
+sleep 2
+echo "-----------------------------------------------------------------------------------"
+echo "opening docker bash"
+echo "-----------------------------------------------------------------------------------"
+echo ""
+echo "you are in directory /home/condauser/tutorials"
+echo ""
+echo "type 'exit' when finished to exit docker environment"
+echo ""
+docker exec -it data_recipes bash
+echo ""
+echo "-----------------------------------------------------------------------------------"
+echo "closing jupyter notebook"
+echo "-----------------------------------------------------------------------------------"
+echo ""
+docker stop data_recipes > /dev/null 2>&1
+echo "-----------------------------------------------------------------------------------"
+echo "closing docker container"
+echo "-----------------------------------------------------------------------------------"
+echo ""
