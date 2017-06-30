1877b0caca1c39ba60348b445504f91b206230b4
karthenjamin <karthenjamin@gmail.com>
2017-06-30 09:52:59 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,10 +4,10 @@ echo ""
 
 docker stop $(docker ps -q) > /dev/null 2>&1
 docker rm data_recipes > /dev/null 2>&1
-docker rmi -f bbanavige/data_recipes > /dev/null 2>&1
+docker rmi -f karthenjamin/data_recipes > /dev/null 2>&1
 
 echo ""
 echo "installing new image"
 echo ""
 
-docker pull bbanavige/data_recipes
+docker pull karthenjamin/data_recipes
